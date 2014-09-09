71feee7f504799995381d199e35b688a72845a70
Jeffrey Wong <jvwong@outlook.com>
2014-09-09 13:44:20 -0400

    static collection includes js/lib now

--- fabfile.py
+++ fabfile.py
@@ -93,7 +93,7 @@ def _update_static_files(js_dir, static_dir, source_dir):
     run('cd %s && npm install && bower install' % (js_dir,))
     run('cd %s &&  lessc -x less/app.less css/base.css' % (static_dir,))
     run('cd %s &&  browserify src/main.js -o dist/bundle.js' % (js_dir,))
-    run('cd %s && ../virtualenv/bin/python3.4 manage.py collectstatic --clear --noinput -i node_modules -i less -i lib -i src -i *.json -i .bowerrc' % (source_dir, ))
+    run('cd %s && ../virtualenv/bin/python3.4 manage.py collectstatic --clear --noinput -i node_modules -i less -i src -i *.json -i .bowerrc' % (source_dir, ))
 
 
 def _update_database(source_dir):
