5e800612326dc45679c9d22445501b402a673594
Jeffrey Wong <jvwong@outlook.com>
2014-09-18 12:43:40 -0400

    reduced asset image test size; throttling bug

--- config/settings.py
+++ config/settings.py
@@ -170,6 +170,13 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication'
     ),
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.UserRateThrottle',
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'user': '1000/day',
+
+    },
     'PAGINATE_BY': 50,
 }
 
