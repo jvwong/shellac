e184fa1b4e23c921cb5e1d0a2127128426f1ed6b
Jeffrey Wong <jvwong@outlook.com>
2014-10-06 15:49:49 -0400

    styling

--- shellac/static/shellac/js/src/util.js
+++ shellac/static/shellac/js/src/util.js
@@ -76,13 +76,7 @@ var util = (function () {
     // Throws    : none
     truncate = function(string, maxchar){
         var str = string || '';
-
-        console.log(str);
-        console.log(maxchar);
-
         var truncated = str.slice(0, maxchar);
-        console.log(truncated);
-
         if(str.length > maxchar){
             truncated += "...";
         }
