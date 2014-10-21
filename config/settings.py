75ddde6d022df00b2782762ea48d7a1d4d223f92
Jeffrey Wong <jvwong@outlook.com>
2014-10-21 19:08:56 -0400

    update

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ DATABASES = {
     }
 }
 
-
+print(os.getenv('MYSQL_SHELLAC_USER', default=''))
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ['*']
