50daa2491b03cb1e77c3dde88c4d131f7a229227
Jeffrey Wong <jvwong@outlook.com>
2014-10-29 14:50:20 -0400

    added siren test files

---
+++ shellac/static/shellac/js/src/siren/bufferLoader.js
@@ -0,0 +1,56 @@
+/**
+ * BufferLoader
+ */
+function BufferLoader(context, urlList, callback) {
+    this.context = context;
+    this.urlList = urlList;
+    this.onload = callback; //one callback to rule them all
+    this.bufferList = new Array();
+    this.loadCount = 0;
+}
+
+/**
+ *
+ */
+BufferLoader.prototype.loadBuffer = function(url, index) {
+    // Load buffer asynchronously
+    var request = new XMLHttpRequest();
+    request.open("GET", url, true);
+    request.responseType = "arraybuffer";
+
+    var loader = this;
+
+    request.onload = function() {
+        // Asynchronously decode the audio file data in request.response
+        loader.context.decodeAudioData(
+            request.response,
+            function(buffer) {
+                if (!buffer) {
+                    alert('error decoding file data: ' + url);
+                    return;
+                }
+                loader.bufferList[index] = buffer;
+                if (++loader.loadCount == loader.urlList.length)
+                    loader.onload(loader.bufferList);
+            },
+            function(error) {
+                console.error('decodeAudioData error', error);
+            }
+        )
+    };
+
+    request.onerror = function() {
+        alert('BufferLoader: XHR error');
+    };
+
+    request.send();
+};
+
+/**
+ * load Fires the loadBuffer() function for each url in the list
+ */
+BufferLoader.prototype.load = function() {
+    for (var i = 0; i < this.urlList.length; ++i)
+        this.loadBuffer(this.urlList[i], i);
+};
+
